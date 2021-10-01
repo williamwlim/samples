ab853596369abc50af11834a513a9fa4caf4d8ac
williamwlim <78177251+williamwlim@users.noreply.github.com>
2021-09-30 18:12:28 -0700

    Add files via upload

---
+++ FileRenameApp/fileRenameTool.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# fileRenameTool.py
+
+"""This module provides the Rename File Tool entry point script."""
+
+from rename.app import main
+
+if __name__ == "__main__":
+    main()
